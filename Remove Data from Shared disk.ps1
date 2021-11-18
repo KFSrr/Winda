7bb32394f122f82eeaebe66d5c5c5d958817765d
KFSrr <59728001+KFSrr@users.noreply.github.com>
2021-11-18 22:30:56 +0100

    Add files via upload

---
+++ Remove Data from Shared disk.ps1
@@ -0,0 +1,7 @@
+﻿takeown /f "c:\folder\subfolder" /r
+$mypath = "D:\Install\VPN\Configs"
+$myacl = Get-Acl $mypath
+$myaclentry = "f.username","FullControl","Allow"
+$myaccessrule = New-Object System.Security.AccessControl.FileSystemAccessRule($myaclentry)
+$myacl.SetAccessRule($myaccessrule)
+Get-ChildItem -Path "$mypath" -Recurse -Force | Set-Acl -AclObject $myacl -Verbose
